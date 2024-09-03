d4c1235be9afb790e678a07bfc203335e0e69e9a
Darren Truong <128624091+darren-truong@users.noreply.github.com>
2024-09-02 18:42:13 -0700

    Complete exercise 1.11

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -15,17 +15,27 @@ const Statistics = ({ good, neutral, bad }) => {
   return (
     <>
       <h2>statistics</h2>
-      <StatisticLine text="good" value={good} />
-      <StatisticLine text="neutral" value={neutral} />
-      <StatisticLine text="bad" value={bad} />
-      <StatisticLine text="all" value={good + neutral + bad} />
-      <StatisticLine text="average" value={(good - bad) / (good + neutral + bad)} />
-      <StatisticLine text="positive" value={(100 * good) / (good + neutral + bad) + ' %'} />
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={good} />
+          <StatisticLine text="neutral" value={neutral} />
+          <StatisticLine text="bad" value={bad} />
+          <StatisticLine text="all" value={good + neutral + bad} />
+          <StatisticLine text="average" value={(good - bad) / (good + neutral + bad)} />
+          <StatisticLine text="positive" value={(100 * good) / (good + neutral + bad) + ' %'} />
+        </tbody>
+      </table>
     </> 
   )
 }
+
 const StatisticLine = ({ text, value }) => {
-return <p>{text} {value} </p>
+return (
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
+)
 }
 
 const App = () => {
