6b80949a53b72ad804437be059b48d6e28e0e06f
Darren Truong <128624091+darren-truong@users.noreply.github.com>
2024-09-08 17:05:55 -0700

    Complete exercise 3.11

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -5,6 +5,7 @@ const morgan = require('morgan')
   
 const app = express()
 
+app.use(express.static('dist'))
 app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
