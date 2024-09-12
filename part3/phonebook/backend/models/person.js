6d78c0af9e474e25584d43f094eedccb54b688e3
Darren Truong <128624091+darren-truong@users.noreply.github.com>
2024-09-12 16:04:17 -0700

    Complete exercise 3.20

--- part3/phonebook/backend/models/person.js
+++ part3/phonebook/backend/models/person.js
@@ -4,26 +4,36 @@ const url = process.env.MONGODB_URI
 
 mongoose.set('strictQuery', false)
 mongoose.connect(url).then(response => {
-    console.log('connected to MongoDB')
+  console.log('connected to MongoDB')
 }).catch(error => {
-    console.log('error connecting to MongoDB:', error.message)
+  console.log('error connecting to MongoDB:', error.message)
 })
 
 const personSchema = new mongoose.Schema({
-    name: {
-      type: String,
-      minLength: 3,
-      required: true
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 3
+  },
+  number: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (v) {
+        return /^(0\d{1,2}-\d{6,})$/.test(v)
+      },
+      message: props => `${props.value} is not a valid phone number!`
     },
-    number: String,
+  }
 })
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject.__v
-    }
-  })
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
 
 module.exports = mongoose.model('Person', personSchema)
