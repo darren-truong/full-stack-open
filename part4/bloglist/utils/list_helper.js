4390844e6b4e7832f211803c10f661dc68cd8254
Darren Truong <128624091+darren-truong@users.noreply.github.com>
2024-09-14 15:59:15 -0700

    Complete exercise 4.5

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,12 +1,33 @@
 const dummy = (blogs) => {
-    return 1
+  return 1
 }
 
 const totalLikes = (blogs) => {
-    return blogs.reduce((sum, current) => sum + current.likes, 0)
+  return blogs.reduce((sum, current) => sum + current.likes, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  return blogs.reduce((greatest, current) => {
+    if (Object.keys(greatest).length === 0) {
+      return {
+        title: current.title,
+        author: current.author,
+        likes: current.likes
+      }
+    }
+    if (current.likes > greatest.likes) {
+      return {
+        title: current.title,
+        author: current.author,
+        likes: current.likes
+      }
+    }
+    return greatest
+  }, {}) 
 }
 
 module.exports = {
-    dummy,
-    totalLikes
+  dummy,
+  totalLikes,
+  favoriteBlog
 }
