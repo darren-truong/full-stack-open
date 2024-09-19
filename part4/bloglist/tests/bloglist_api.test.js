621fe339de577dd696044eec3013ee0b1f0d9088
Darren Truong <128624091+darren-truong@users.noreply.github.com>
2024-09-19 15:35:14 -0700

    Complete exercise 4.10

--- part4/bloglist/tests/bloglist_api.test.js
+++ part4/bloglist/tests/bloglist_api.test.js
@@ -38,6 +38,33 @@ describe('bloglist api:', () => {
     assert("id" in blogs.body[0])
   })
 
+  test('new blog post successfully saved', async () => {
+    const newBlog = {
+      title: "New Blog",
+      author: "Dewey Alvin",
+      url: "http://newblogwebsite.html",
+      likes: 20
+    }
+  
+    const response = await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+  
+    // Access the returned object
+    const returnedBlog = response.body
+  
+    // You can now assert on the returned object if needed
+    assert.strictEqual(returnedBlog.title, newBlog.title)
+    assert.strictEqual(returnedBlog.author, newBlog.author)
+    assert.strictEqual(returnedBlog.url, newBlog.url)
+    assert.strictEqual(returnedBlog.likes, newBlog.likes)
+  
+    const blogsAtEnd = await api.get('/api/blogs')
+    assert.strictEqual(blogsAtEnd.body.length, helper.initialBlogs.length + 1)
+  })
+
   after(async () => {
     await mongoose.connection.close()
   })
