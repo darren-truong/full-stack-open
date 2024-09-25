359e8a6cd31d0ecba385cb70674c21aaaa1f24c6
Darren Truong <128624091+darren-truong@users.noreply.github.com>
2024-09-24 22:07:00 -0700

    Complete exercise 4.20

--- part4/bloglist/utils/middleware.js
+++ part4/bloglist/utils/middleware.js
@@ -8,6 +8,16 @@ const requestLogger = (request, response, next) => {
   next()
 }
 
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    request.token = authorization.replace('Bearer ', '')
+  } else {
+    request.token = null
+  }
+  next() 
+}
+
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
@@ -21,7 +31,7 @@ const errorHandler = (error, request, response, next) => {
     return response.status(400).json({ error: error.message })
   } else if (error.name === 'MongoServerError' && error.message.includes('E11000 duplicate key error')) {
     return response.status(400).json({ error: 'expected `username` to be unique' })
-  } else if (error.name ===  'JsonWebTokenError') {
+  } else if (error.name === 'JsonWebTokenError') {
     return response.status(401).json({ error: 'token invalid' })
   }
 
@@ -30,6 +40,7 @@ const errorHandler = (error, request, response, next) => {
 
 module.exports = {
   requestLogger,
+  tokenExtractor,
   unknownEndpoint,
-  errorHandler
+  errorHandler,
 }
