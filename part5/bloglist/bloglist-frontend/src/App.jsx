2a817d869a21cc80a8680cd3e54c1a2326292a65
Darren Truong <128624091+darren-truong@users.noreply.github.com>
2024-09-30 17:52:40 -0700

    Complete exercise 5.2

--- part5/bloglist/bloglist-frontend/src/App.jsx
+++ part5/bloglist/bloglist-frontend/src/App.jsx
@@ -15,6 +15,14 @@ const App = () => {
     )
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedNoteappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+    }
+  }, [])
+
   const handleLogin = async (event) => {
     event.preventDefault()
 
@@ -22,11 +30,19 @@ const App = () => {
       const user = await loginService.login({
         username, password
       })
+      window.localStorage.setItem(
+        'loggedNoteappUser', JSON.stringify(user)
+      )
       setUser(user)
       setUsername('')
       setPassword('')
     } catch (exception) {
     }
+  }
+
+  const handleLogout = async (event) => {
+    window.localStorage.clear()
+    setUser(null)
   }
 
   if (user === null) {
@@ -61,7 +77,7 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
-      <p>{user.name} logged in</p>
+      <p>{user.name} logged in <button onClick={handleLogout}>logout</button></p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
